3e6d84e872d7b990a8604be533bb137b6818232d
NfN Orange <orange@ff4500.red>
2019-12-09 00:00:00 +0000

    Mon  9 Dec 00:00:00 GMT 2019

--- Adjust.sh
+++ Adjust.sh
@@ -5,13 +5,15 @@
 # Read this first: https://blog.jcoglan.com/2013/11/15/why-github-is-not-your-cv/
 
 adjust() {
-	date -d "$1" +%s | xargs -I% sudo date -s "@%"
+    NEXTDATE=$(date -d "$LASTDATE")
 	echo "$RANDOM" | tee nonce
 	git add nonce
-	git commit -am "adjusting nonce"
+	git commit -am "$NEXTDATE"
+    GIT_COMMITTER_DATE="Mon 20 Aug 2018 20:19:19 BST" git commit --amend --no-edit --date "$NEXTDATE"
+    LASTDATE=$NEXTDATE
 }
 
-adjust "29 June 2013"
+adjust "25 May 2018"
 
 for i in $(seq 0 1789); do
 	echo $i
